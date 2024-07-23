681e9faa4aac8900410eabe7b93ed6f28196d30d
Alexandre Machado <alexandre@machado.cc>
2024-07-23 05:40:41 +0000

    chore: Add code to connect to local SQL Server and create a sample database

--- dotnet-guidv7/Program.cs
+++ dotnet-guidv7/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+// connect on local sql server
+string connectionString = "Server=localhost;Database=master;User Id=sa;Password=3uuiCaKxfbForrK;";
+using (SqlConnection connection = new SqlConnection(connectionString))
+{
+    connection.Open();
+    Console.WriteLine("Connected to SQL Server");
+
+    // Create a sample database
+    String sql = "CREATE DATABASE SampleDB";
+    using (SqlCommand command = new SqlCommand(sql, connection))
+    {
+        command.ExecuteNonQuery();
+        Console.WriteLine("Created Database");
+    }
+}
