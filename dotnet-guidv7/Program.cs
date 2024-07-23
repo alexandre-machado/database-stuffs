405d39e627eea7237bc145f863f54aee2311456d
Alexandre Machado <alexandre@machado.cc>
2024-07-23 05:57:25 +0000

    chore: Add code to connect to local SQL Server and create a sample database

--- dotnet-guidv7/Program.cs
+++ dotnet-guidv7/Program.cs
@@ -1,17 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.Data.SqlClient;
+
 Console.WriteLine("Hello, World!");
-// connect on local sql server
-string connectionString = "Server=localhost;Database=master;User Id=sa;Password=3uuiCaKxfbForrK;";
-using (SqlConnection connection = new SqlConnection(connectionString))
+
+var connectionString = "Server=localhost;Database=master;User Id=sa;Password=3uuiCaKxfbForrK;TrustServerCertificate=true;";
+using (var connection = new SqlConnection(connectionString))
 {
     connection.Open();
     Console.WriteLine("Connected to SQL Server");
+    // drop the database if it exists
+    var sql = "DROP DATABASE IF EXISTS SampleDB";
+    using var command = new SqlCommand(sql, connection);
+    command.ExecuteNonQuery();
+    Console.WriteLine("Dropped Database");
 
     // Create a sample database
-    String sql = "CREATE DATABASE SampleDB";
-    using (SqlCommand command = new SqlCommand(sql, connection))
-    {
-        command.ExecuteNonQuery();
-        Console.WriteLine("Created Database");
-    }
+    sql = "CREATE DATABASE SampleDB";
+    command.CommandText = sql;
+    command.ExecuteNonQuery();
+    Console.WriteLine("Created Database");
+
+    // create a table
+    sql = @"
+        USE SampleDB;
+        CREATE TABLE Inventory (
+            Id INT PRIMARY KEY IDENTITY,
+            Name NVARCHAR(50),
+            Quantity INT
+        );
+    ";
+    command.CommandText = sql;
+    command.ExecuteNonQuery();
+    Console.WriteLine("Created Table");
 }
+// docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=3uuiCaKxfbForrK" -p 1433:1433 -d mcr.microsoft.com/mssql/server:2022-latest
